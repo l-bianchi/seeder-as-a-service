05998bb68c51aeb1c8cf56cc1a0335046069a09a
l-bianchi <illorenzobianchi@proton.me>
2023-11-15 18:11:39 +0100

    add stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
-	import { Input, Button, Select, Breadcrumb, BreadcrumbItem } from 'flowbite-svelte';
+	import {
+		Input,
+		GradientButton,
+		Button,
+		Badge,
+		Breadcrumb,
+		BreadcrumbItem,
+		Modal
+	} from 'flowbite-svelte';
 	import { PlusOutline } from 'flowbite-svelte-icons';
 
 	const types: { value: string; name: string }[] = [
@@ -12,54 +20,73 @@
 	];
 
 	let current: string = 'root';
-	let nodes: any = { root: { nav: 'root', raws: 1, types: [], inputs: [] } };
+	let nodes: any = { root: { path: ['root'], list: [] } };
+	let showModal = false;
 
-	function goDeep(newNode: string) {
+	function addRaw(type: string) {
+		nodes[current].list = [...nodes[current].list, { type, value: '' }];
+		showModal = false;
+	}
+
+	function goDeeper(newNode: string) {
 		if (!nodes[newNode]) {
 			nodes[newNode] = {
-				nav: `${nodes[current].nav}/${newNode}`,
-				raws: 1,
-				types: [],
-				inputs: []
+				path: [...nodes[current].path, newNode],
+				list: []
 			};
 		}
 		current = newNode;
 	}
 </script>
 
-<div class="p-4">
-	<Breadcrumb solid>
-		{#each nodes[current].nav.split('/') as depth}
-			<BreadcrumbItem home={depth === 'root'}>
-				<Button
-					color="none"
-					class="p-0 bg-transparent text-lg text-bold focus:ring-0"
-					on:click={() => (current = depth)}
-				>
-					{depth}
-				</Button>
-			</BreadcrumbItem>
-		{/each}
-	</Breadcrumb>
+<div class="w-screen h-screen dark:bg-gray-900">
+	<div class="p-4">
+		<Breadcrumb solid>
+			{#each nodes[current].path as path}
+				<BreadcrumbItem home={path === 'root'}>
+					<Button
+						color="none"
+						class="p-0 bg-transparent text-lg text-bold focus:ring-0"
+						on:click={() => (current = path)}
+					>
+						{path}
+					</Button>
+				</BreadcrumbItem>
+			{/each}
+		</Breadcrumb>
+	</div>
+	<div class="flex flex-col p-4 space-y-4">
+		{#key current}
+			{#each nodes[current].list as { type, value, color }}
+				<div class="flex w-full space-x-4">
+					<Badge border color="purple">{type}</Badge>
+					<Input placeholder="Type field name" bind:value />
+					{#if type === 'object'}
+						<GradientButton outline shadow color="purpleToPink" on:click={() => goDeeper(value)}>
+							<p class="text-bold uppercase">open</p>
+						</GradientButton>
+					{/if}
+				</div>
+			{/each}
+		{/key}
+		<GradientButton
+			outline
+			shadow
+			color="purpleToPink"
+			class="w-fit self-end"
+			on:click={() => (showModal = true)}
+		>
+			<PlusOutline class="w-5 h-5 outline-none" />
+		</GradientButton>
+	</div>
 </div>
-<div class="p-4 space-y-4">
-	{#key current}
-		{#each [...Array(nodes[current].raws).keys()] as _, index}
-			<div class="flex w-full space-x-4">
-				<Select
-					class="w-fit uppercase"
-					placeholder=""
-					items={types}
-					bind:value={nodes[current].types[index]}
-				/>
-				<Input placeholder="Type field name" bind:value={nodes[current].inputs[index]} />
-				{#if nodes[current].types[index] === 'object'}
-					<Button on:click={() => goDeep(nodes[current].inputs[index])}>open</Button>
-				{/if}
-			</div>
+
+<Modal title="Select a type" bind:open={showModal} autoclose outsideclose>
+	<div class="grid grid-cols-3 gap-4">
+		{#each types as { value, name }}
+			<GradientButton outline shadow color="purpleToPink" on:click={() => addRaw(value)}>
+				<p class="text-lg text-bold uppercase">{name}</p>
+			</GradientButton>
 		{/each}
-		<Button on:click={() => nodes[current].raws++}>
-			<PlusOutline class="w-5 h-5" />
-		</Button>
-	{/key}
-</div>
+	</div>
+</Modal>
